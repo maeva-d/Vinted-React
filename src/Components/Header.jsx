74ffc4bb54706445d524ed9a5d8dbf35257fe7e9
Maeva DTC <delruemaeva@orange.fr>
2024-08-29 18:19:43 +0200

    added useNavigate in header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,14 +1,17 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
   const [signup, setSignup] = useState(false);
   const [login, setLogin] = useState(false);
   const [sellArticle, setSellArticle] = useState(false);
 
+  const navigate = useNavigate();
+
   return (
     <header className="container">
       <img src="\src\assets\logo-vinted.png" />
-      <button onClick={setSignup(true)}>S'inscrire</button>
+      <button onClick={() => navigate("/Signup")}>S'inscrire</button>
       <button onClick={setLogin(true)}>Se connecter</button>
       <button onClick={setSellArticle(true)}>Vends tes articles</button>
     </header>
