7145e2407c8baf466211183e35494e092fa43c0a
Maeva D <delruemaeva@orange.fr>
2024-10-23 18:20:58 +0200

    wip Vends tes articles page

--- src/pages/login-page/Login.jsx
+++ src/pages/login-page/Login.jsx
@@ -1,60 +1,71 @@
 import "./login.scss";
-import axios from "axios";
-import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+// import "../../Components/header-component/main-modals.scss";
+// import axios from "axios";
+// import { useState } from "react";
+// import { useNavigate } from "react-router-dom";
+
+// import cross from "../../assets/cross.svg";
+// import LogInModal from "../../Components/header-component/LogInModal";
+import Modals from "../../Components/header-component/Modals";
 
 // Je récupère la fonction handleToken en props
 const Login = ({ handleToken }) => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  // const [showRedirection, setShowRedirection] = useState(true);
+  // const [showLogin, setShowLogin] = useState(false);
+  // const [showSignUp, setShowSignUp] = useState(false);
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
 
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      const response = await axios.post(
-        "https://lereacteur-vinted-api.herokuapp.com/user/login",
-        {
-          email: email,
-          password: password,
-        }
-      );
-      console.log(response.data);
-      // Si ça a fonctionné, la réponse me renvoie le token:
-      handleToken(response.data.token);
-      navigate("/");
-    } catch (error) {
-      console.log(error.response.data);
-    }
-  };
+  // const handleSubmit = async (event) => {
+  //   event.preventDefault();
+  //   try {
+  //     const response = await axios.post(
+  //       "https://lereacteur-vinted-api.herokuapp.com/user/login",
+  //       {
+  //         email: email,
+  //         password: password,
+  //       }
+  //     );
+  //     console.log(response.data);
+  //     // Si ça a fonctionné, la réponse me renvoie le token:
+  //     handleToken(response.data.token);
+  //     navigate("/");
+  //   } catch (error) {
+  //     console.log(error.response.data);
+  //   }
+  // };
 
   return (
-    <main className="login-container">
-      <h1>Rejoins le mouvement de la seconde main et vends sans frais!</h1>
-      <form onSubmit={handleSubmit} className="login-form">
-        <input
-          className="login-input"
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(event) => {
-            setEmail(event.target.value);
-          }}
-        />
-        <input
-          className="login-input"
-          type="password"
-          placeholder="Mot de passe"
-          value={password}
-          onChange={(event) => {
-            setPassword(event.target.value);
-          }}
-        />
-        <button className="login-button">Se connecter</button>
-      </form>
-      <Link to="/signup">Pas encore de compte ? Inscris-toi !</Link>
+    <main>
+      <Modals handleToken={handleToken} />
     </main>
+    // <main className="modals">
+    // <h1>Rejoins le mouvement de la seconde main et vends sans frais!</h1>
+    // <form onSubmit={handleSubmit} className="login-form">
+    //   <input
+    //     className="login-input"
+    //     type="email"
+    //     placeholder="Email"
+    //     value={email}
+    //     onChange={(event) => {
+    //       setEmail(event.target.value);
+    //     }}
+    //   />
+    //   <input
+    //     className="login-input"
+    //     type="password"
+    //     placeholder="Mot de passe"
+    //     value={password}
+    //     onChange={(event) => {
+    //       setPassword(event.target.value);
+    //     }}
+    //   />
+    //   <button className="login-button">Se connecter</button>
+    // </form>
+    // <Link to="/signup">Pas encore de compte ? Inscris-toi !</Link> */}
+    // </main>
   );
 };
 
